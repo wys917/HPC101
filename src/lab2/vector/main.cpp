7c22bdb6db82eda19d6f9be8e838aec5f4856a97
CHEN HONGZHE <84257699+Chenhzjs@users.noreply.github.com>
2025-07-01 16:24:45 +0800

    Update main.cpp

--- src/lab2/vector/main.cpp
+++ src/lab2/vector/main.cpp
@@ -15,7 +15,7 @@ void naive_gemm(uint8_t* A, int8_t* B, uint32_t* C, int m, int n, int k) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             for (int kk = 0; kk < k * 4; kk++) {
-                C[i * k + j] += A[i * k * 4 + kk] * B[j * k * 4 + kk];
+                C[i * n + j] += A[i * k * 4 + kk] * B[j * k * 4 + kk];
             }
         }
     }
